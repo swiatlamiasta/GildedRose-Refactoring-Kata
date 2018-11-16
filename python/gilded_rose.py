86fd840d4c9ac795209e4785e4ab23e2efb1f151
Artur Golebiewski <artgolebiewski@gmail.com>
2018-11-16 18:57:54 +0100

    tests + first naive fix

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import re
 
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+    def update_quality_original(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
@@ -35,6 +36,36 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
+    def update_quality(self):
+        '''First naive non OOP attempt
+        '''
+
+        for item in self.items:
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                item.quality = item.quality
+                item.sell_in = item.sell_in
+            elif item.name == "Aged Brie":
+                if item.quality < 50:
+                    item.quality += 1
+                item.sell_in -= 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:   
+                    if item.sell_in < 1:
+                        item.quality = 0
+                    elif item.sell_in < 6:
+                        item.quality += 3
+                    elif item.sell_in < 11:
+                        item.quality += 2
+                    else:
+                        item.quality += 1
+                item.sell_in -= 1
+            else:
+                if 0 < item.quality < 50:
+                    if item.sell_in < 1:
+                        item.quality -= 2
+                    else:
+                        item.quality -= 1
+                item.sell_in -= 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -8,7 +8,81 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+    
+    def test_normal_item_quality_and_expiration_date(self):
+        items = [Item("Random item", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(9, items[0].quality)
+        self.assertEqual(4, items[0].sell_in)
+
+    def test_normal_item_quality_after_expiration_date(self):
+        items = [Item("Random item", -1, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(8, items[0].quality)
+        self.assertEqual(-2, items[0].sell_in)
+
+    def test_normal_item_quality_never_negative(self):
+        items = [Item("Random item", 5, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+        self.assertEqual(4, items[0].sell_in)
+
+    def test_quality_goes_up_for_improving_products(self):
+        items = []
+        items.append(Item("Aged Brie", 20, 30))
+        items.append(Item("Backstage passes to a TAFKAL80ETC concert", 15, 20))
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        expected = [
+              {'sell_in': 19, 'quality': 31},
+              {'sell_in': 14, 'quality': 21},
+            ]
+
+        for index, expectation in enumerate(expected):
+            item = items[index]
+            self.assertEqual(item.quality, expectation['quality'])
+            self.assertEqual(item.sell_in, expectation['sell_in'])
+
+    def test_quality_never_higher_than_50(self):
+        items = [Item("Aged Brie", 5, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+        self.assertEqual(4, items[0].sell_in)
+
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 10, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(10, items[0].quality)
+        self.assertEqual(10, items[0].sell_in)
+
+    def test_backstage_pass_within_10_days(self):
+        items = []
+        items.append(Item("Backstage passes to a TAFKAL80ETC concert", 5, 20))
+        items.append(Item("Backstage passes to a TAFKAL80ETC concert", 10, 10))
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        expected = [
+              {'sell_in': 4, 'quality': 23},
+              {'sell_in': 9, 'quality': 12},
+            ]
+
+        for index, expectation in enumerate(expected):
+            item = items[index]
+            self.assertEqual(item.quality, expectation['quality'])
+            self.assertEqual(item.sell_in, expectation['sell_in'])
+
+    def test_pass_after_expiration_date(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+        self.assertEqual(-1, items[0].sell_in)
 
 if __name__ == '__main__':
     unittest.main()
